36b213f0c661c81e73d4f32920a3237984c9c1e7
eduardo <eduardo-ds@hotmail.com>
2018-01-17 01:52:19 -0200

    Add project files.

---
+++ src/TopshelfDemo.ConsoleApp/Entities/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopshelfDemo.ConsoleApp.Entities
+{
+    public class Startup
+    {
+        readonly System.Timers.Timer _timer;
+
+        public Startup()
+        {
+            _timer = new System.Timers.Timer( /* ConverterMinToMiliseconds(1) */ ) { AutoReset = false };
+            _timer.Elapsed += (sender, eventArgs) => Run();
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+
+        public void Run()
+        {
+            //do something!
+            Console.WriteLine();
+            Console.WriteLine(new String('*', 50));
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(new String('*', 50));
+        }
+
+        public double ConverterMinToMiliseconds(double valor)
+        {
+            return valor * 60000;
+        }
+    }
+}
---
+++ src/TopshelfDemo.ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+using TopshelfDemo.ConsoleApp.Entities;
+
+namespace TopshelfDemo.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(r =>
+            {
+                r.Service<Startup>(s =>
+                {
+                    s.ConstructUsing(name => new Startup());
+                    s.WhenStarted(ws => ws.Start());
+                    s.WhenStopped(ws => ws.Stop());
+                });
+
+                r.RunAsLocalSystem();
+
+                r.SetDescription("Descricao do windows service");
+                r.SetDisplayName("Nome de exibicao windows service");
+                r.SetServiceName("Nome do windows service");
+
+            });
+        }
+    }
+}
